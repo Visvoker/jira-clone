b2091f1265bbb5268263e136e3d6b8a44d843578
visvoker <s2089533@gmail.com>
2024-10-16 11:05:24 +0800

    Creating auth screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
-import { Button } from "@/components/ui/button";
-import { TestComponent } from "@/features/test";
 
 
 export default function Home() {
   return (
     <div>
-      <div>
-        <TestComponent />
-      </div>
-      <Button>
-        Click me
-      </Button>
+      Hello
     </div>
   );
 }
