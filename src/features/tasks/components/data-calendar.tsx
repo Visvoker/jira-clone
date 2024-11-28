020577eb4790aa6c2aedbdf0aae9f279c40cfa82
visvoker <s2089533@gmail.com>
2024-11-28 20:13:44 +0800

    Building a data calendar

--- src/features/tasks/components/data-calendar.tsx
+++ src/features/tasks/components/data-calendar.tsx
@@ -11,6 +11,10 @@ import {
 import { enUS } from "date-fns/locale";
 import { Calendar, dateFnsLocalizer } from "react-big-calendar";
 
+import { Button } from "@/components/ui/button";
+
+import { EventCard } from "./event-card";
+
 import { Task } from "../types"
 
 import "react-big-calendar/lib/css/react-big-calendar.css"
@@ -31,6 +35,36 @@ const localizer = dateFnsLocalizer({
 interface DataCalendarProps {
   data: Task[];
 };
+
+interface CustomToolbarProps {
+  date: Date;
+  onNavigate: (action: "PREV" | "NEXT" | "TODAY") => void;
+};
+
+const CustomToolbar = ({ date, onNavigate }: CustomToolbarProps) => {
+  return (
+    <div className="flex mb-4 gap-x-2 items-center w-full lg:w-auto justify-center lg:justify-start">
+      <Button
+        onClick={() => onNavigate("PREV")}
+        variant="secondary"
+        size="icon"
+      >
+        <ChevronLeftIcon className="size-4" />
+      </Button>
+      <div className="flex items-center border border-input rounded-md px-3 py-2 h-8 justify-center w-full lg:w-auto">
+        <CalendarIcon className="size-4 mr-2" />
+        <p className="text-sm">{format(date, "MMMM yyyy")}</p>
+      </div>
+      <Button
+        onClick={() => onNavigate("NEXT")}
+        variant="secondary"
+        size="icon"
+      >
+        <ChevronRightIcon className="size-4" />
+      </Button>
+    </div>
+  )
+}
 
 export const DataCalendar = ({ data }: DataCalendarProps) => {
   const [value, setValue] = useState(
@@ -70,6 +104,20 @@ export const DataCalendar = ({ data }: DataCalendarProps) => {
       max={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
       formats={{
         weekdayFormat: (date, culture, localizer) => localizer?.format(date, "EEE", culture) ?? ""
+      }}
+      components={{
+        eventWrapper: ({ event }) => (
+          <EventCard
+            id={event.id}
+            title={event.title}
+            assignee={event.assignee}
+            project={event.project}
+            status={event.status}
+          />
+        ),
+        toolbar: () => (
+          <CustomToolbar date={value} onNavigate={handleNavigate} />
+        )
       }}
     />
   )
